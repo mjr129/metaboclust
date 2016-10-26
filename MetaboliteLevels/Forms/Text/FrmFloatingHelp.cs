2461de83326a98f717c81feaf55b37a615c5d7ad
mjr129 <mjr129@york.ac.uk>
2016-10-26 20:12:48 +0100

    new context help

---
+++ MetaboliteLevels/Forms/Text/FrmFloatingHelp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetaboliteLevels.Forms.Text
+{
+    public partial class FrmFloatingHelp : Form
+    {
+        public FrmFloatingHelp()
+        {
+            InitializeComponent();
+        }
+
+        internal void SetText( string text )
+        {
+            textBox1.Text = text;
+        }
+    }
+}
